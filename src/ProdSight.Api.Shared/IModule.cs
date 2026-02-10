ac5a288a5637c33909751fa208d336cd7d8775d9
Paweł Worwąg <pawel.worwag@gmail.com>
2026-02-11 00:22:17 +0100

    Add `RequiredScope` property to `IModule` interface for identity provider integration

--- src/ProdSight.Api.Shared/IModule.cs
+++ src/ProdSight.Api.Shared/IModule.cs
@@ -6,6 +6,7 @@ namespace ProdSight.Api.Shared;
 public interface IModule
 {
     string Name { get; }
+    string RequiredScope { get; }  // Scope required from identity provider (e.g., Keycloak)
     void RegisterServices(IServiceCollection services);
     void ConfigureEndpoints(IEndpointRouteBuilder endpoints);
 }
