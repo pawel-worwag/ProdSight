e0c94103878117cee3842a8b6198b424a8cc6783
Paweł Worwąg <pawel.worwag@gmail.com>
2026-02-13 22:46:52 +0100

    Dynamically load module assemblies from `publish/modules` to enable runtime type resolution and improve module extensibility. Adjust `AddModules` to handle directory scanning and assembly loading.

--- src/ProdSight.Api/Extensions/ModuleExtensions.cs
+++ src/ProdSight.Api/Extensions/ModuleExtensions.cs
@@ -1,3 +1,8 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 using ProdSight.Api.Shared.Modules;
 
 namespace ProdSight.Api.Extensions;
@@ -6,10 +11,42 @@ public static class ModuleExtensions
 {
     public static IServiceCollection AddModules(this IServiceCollection services, IConfiguration config)
     {
+        // Load module assemblies from publish/modules if present so types referenced in code can be resolved
+        try
+        {
+            var baseDir = AppContext.BaseDirectory ?? AppDomain.CurrentDomain.BaseDirectory;
+            var modulesDir = Path.Combine(baseDir, "modules");
+            if (Directory.Exists(modulesDir))
+            {
+                var dlls = Directory.GetFiles(modulesDir, "*.dll", SearchOption.AllDirectories);
+                foreach (var dll in dlls)
+                {
+                    try
+                    {
+                        var name = Path.GetFileNameWithoutExtension(dll);
+                        var alreadyLoaded = AppDomain.CurrentDomain.GetAssemblies()
+                            .Any(a => string.Equals(a.GetName().Name, name, StringComparison.OrdinalIgnoreCase));
+                        if (!alreadyLoaded)
+                        {
+                            AssemblyLoadContext.Default.LoadFromAssemblyPath(dll);
+                        }
+                    }
+                    catch
+                    {
+                        // ignore individual assembly load errors
+                    }
+                }
+            }
+        }
+        catch
+        {
+            // ignore module loading errors; registration below will fail fast if types missing
+        }
+
         // Register modules explicitly
         services.AddSingleton<IModule, Modules.IdentityModule.Presentation.IdentityModule>();
         services.AddSingleton<IModule, Modules.MeasurementModule.Presentation.MeasurementModule>();
-        
+
         var logger = services.BuildServiceProvider().GetRequiredService<ILogger<ModuleRegistry>>();
         var registry = new ModuleRegistry(logger, services.BuildServiceProvider().GetServices<IModule>());
         registry.LoadModules(services, config);
