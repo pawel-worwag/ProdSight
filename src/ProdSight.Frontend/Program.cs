0694acffc64f0b99f7a05f95f8935f49fcb647f0
Paweł Worwąg <pawel.worwag@gmail.com>
2026-02-14 23:50:05 +0100

    Set up Blazor WebAssembly project with initial structure.

--- src/ProdSight.Frontend/Program.cs
+++ src/ProdSight.Frontend/Program.cs
@@ -1,7 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ProdSight.Frontend;
 
-var app = builder.Build();
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-app.MapGet("/", () => "Hello World");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-app.Run();
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Configure your authentication provider options here.
+    // For more information, see https://aka.ms/blazor-standalone-auth
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+});
+
+await builder.Build().RunAsync();
