372e7caec875aba40810d34502e6c4dc546ea4a5
Paweł Worwąg <pawel.worwag@gmail.com>
2026-02-26 22:57:18 +0100

    Add CORS policy to API with open configuration and integrate into middleware pipeline

--- src/ProdSight.Api/Program.cs
+++ src/ProdSight.Api/Program.cs
@@ -5,17 +5,32 @@ using ProdSight.Api.Middleware;
 using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
-builder.Services.AddHealthChecks(); 
-builder.Services.AddModules(builder.Configuration);
+builder.Services.AddHealthChecks();
+builder.Services.AddModules(configuration);
 builder.Services.AddSingleton<HealthCheckCacheService>();
 builder.Services.AddHostedService(sp => sp.GetRequiredService<HealthCheckCacheService>());
 builder.Services.AddTransient<ExceptionHandlingMiddleware>();
 builder.AddScalar();
 
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ApiCorsPolicy", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
-app.UseModules(builder.Configuration);
+app.UseCors("ApiCorsPolicy");
+
+app.UseModules(configuration);
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.MapStatusEndpoint();
 app.MapHealthEndpoint();
