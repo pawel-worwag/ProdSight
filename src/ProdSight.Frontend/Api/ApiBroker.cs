c0a84485ddfa2c421f9ff76474ee56affe964c26
Paweł Worwąg <pawel.worwag@gmail.com>
2026-02-27 23:40:36 +0100

    Allow authorized and unauthorized api requests.

--- src/ProdSight.Frontend/Api/ApiBroker.cs
+++ src/ProdSight.Frontend/Api/ApiBroker.cs
@@ -5,13 +5,17 @@ using ProdSight.Api.Shared.DTOs.IdentityModule.GetUsersList;
 
 namespace ProdSight.Frontend.Api;
 
-public class ApiBroker(HttpClient httpClient) : IApiBroker
+public class ApiBroker(IHttpClientFactory httpFactory) : IApiBroker
 {
     private static readonly JsonSerializerOptions JsonOptions = new() { PropertyNameCaseInsensitive = true };
+    
+    private HttpClient GetClient(bool requireAuth) =>
+        httpFactory.CreateClient(requireAuth ? "ApiAuthorized" : "Api");
+    
     public async Task<HealthResponse> GetHealthStatusAsync(CancellationToken cancellationToken = default)
     {
         var url = $"health";
-        var res = await httpClient.GetAsync(url, cancellationToken);
+        var res = await GetClient(false).GetAsync(url, cancellationToken);
         res.EnsureSuccessStatusCode();
         return await res.Content.ReadFromJsonAsync<HealthResponse>(JsonOptions, cancellationToken)
             ?? throw new InvalidOperationException("Empty or invalid health response.");
@@ -20,7 +24,7 @@ public class ApiBroker(HttpClient httpClient) : IApiBroker
     public async Task<User> GetAllUsersAsync(CancellationToken cancellationToken = default)
     {
         var url = $"identity/users";
-        var res = await httpClient.GetAsync(url, cancellationToken);
+        var res = await GetClient(true).GetAsync(url, cancellationToken);
         res.EnsureSuccessStatusCode();
         return await res.Content.ReadFromJsonAsync<User>(JsonOptions, cancellationToken)
                ?? throw new InvalidOperationException("Empty or invalid health response.");
--- src/ProdSight.Frontend/Api/Extensions.cs
+++ src/ProdSight.Frontend/Api/Extensions.cs
@@ -9,16 +9,28 @@ public static class Extensions
 
         services.AddTransient<ApiAuthorizationMessageHandler>();
         
-        services.AddHttpClient<IApiBroker, ApiBroker>((sp, client) =>
+        services.AddHttpClient("Api", (sp, client) =>
         {
             var opts = sp.GetRequiredService<Microsoft.Extensions.Options.IOptions<ApiOptions>>().Value;
-
+            if (string.IsNullOrWhiteSpace(opts.BaseUrl))
+            {
+                throw new InvalidOperationException("Configuration 'Backend:BaseUrl' is required for ApiBroker but was not found.");
+            }
+            client.BaseAddress = new Uri(opts.BaseUrl);
+        });
+        
+        services.AddHttpClient("ApiAuthorized", (sp, client) =>
+        {
+            var opts = sp.GetRequiredService<Microsoft.Extensions.Options.IOptions<ApiOptions>>().Value;
             if (string.IsNullOrWhiteSpace(opts.BaseUrl))
             {
                 throw new InvalidOperationException("Configuration 'Backend:BaseUrl' is required for ApiBroker but was not found.");
             }
             client.BaseAddress = new Uri(opts.BaseUrl);
         }).AddHttpMessageHandler<ApiAuthorizationMessageHandler>();
+
+        services.AddScoped<IApiBroker, ApiBroker>();
+        
         return services;
     }
     
