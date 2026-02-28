d5b34cde53a51d68daec4d2ebe3ee473c2463e8a
Paweł Worwąg <pawel.worwag@gmail.com>
2026-02-28 18:22:47 +0100

    Update package versions and improve null safety in `Users.razor.cs` for JS runtime usage

--- src/ProdSight.Frontend/Pages/Identity/Users.razor.cs
+++ src/ProdSight.Frontend/Pages/Identity/Users.razor.cs
@@ -6,18 +6,20 @@ namespace ProdSight.Frontend.Pages.Identity;
 public partial class Users : ComponentBase
 {
     [Inject]
-    private IJSRuntime JS { get; set; }
+    private IJSRuntime? Js { get; set; }
     private IJSObjectReference? _module;
     
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         if (firstRender)
         {
-            _module = await JS.InvokeAsync<IJSObjectReference>(
-                "import",
-                "./js/Identity/Users.js"
-            );
-
+            if (Js is not null)
+            {
+                _module = await Js.InvokeAsync<IJSObjectReference>(
+                    "import",
+                    "./js/Identity/Users.js"
+                );
+            }
         }
     }
 
