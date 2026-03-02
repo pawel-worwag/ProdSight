cbdaa1b6d9dac284bed937470caeed80a4ced734
Paweł Worwąg <pawel.worwag@gmail.com>
2026-03-02 22:27:23 +0100

    Refactor `Users.razor` to delegate row rendering to `UserRow` component.

--- src/ProdSight.Frontend/Components/Identity/UserRow.razor.cs
+++ src/ProdSight.Frontend/Components/Identity/UserRow.razor.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Components;
-
+using ProdSight.Api.Shared.DTOs.IdentityModule.GetUsersList;
 namespace ProdSight.Frontend.Components.Identity;
 
 public partial class UserRow : ComponentBase
 {
+    [Parameter]
+    public required User User { get; set; }
 }
